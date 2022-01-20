7dbd82a49d3e91633d8574e9261e34b0bb0b9fc6
祝奇 <zhuq@meiyibao.com>
2022-01-20 11:11:38 +0800

    init

---
+++ src/libs/LoveMsg/index.ts
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+import dotenv from 'dotenv'
+import { goodMorning } from './goodMorning'
+import { goodAfternoon } from './goodAfternoon'
+import { goodEvening } from './goodEvening'
+dotenv.config()
+
+const { MESSAGE_TYPE } = process.env
+
+export default function main() {
+  if (MESSAGE_TYPE === 'goodAfternoon') {
+    // 午安
+    goodAfternoon()
+  }
+  else if (MESSAGE_TYPE === 'goodEvening') {
+    // 晚安
+    goodEvening()
+  }
+  else {
+    // 早安
+    goodMorning()
+  }
+}
