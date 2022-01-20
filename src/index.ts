ef9e00b88471092557d64735d552114c7584b1d3
wadeazhu <32754963+wadeazhu@users.noreply.github.com>
2022-01-20 11:51:09 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import dotenv from 'dotenv'
 import LoveMsg from './libs/LoveMsg'
 dotenv.config()
+import { wxNotify } from './libs/WxNotify'
+
 
 // 早安、午安、晚安 => 由环境变量控制
-LoveMsg()
+// LoveMsg()
+const template = {
+  msgtype: 'text',
+  text: {
+    content: "message",
+  },
+}
+
+wxNotify(template)
